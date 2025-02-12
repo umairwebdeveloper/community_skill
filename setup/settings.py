c423a0dbac6286df33e2ec3631a62e0a1fad6fdf
Umair Ashraf <umairashraf5252@gmail.com>
2025-02-12 18:41:37 +0500

    fixed

--- setup/settings.py
+++ setup/settings.py
@@ -78,23 +78,23 @@ WSGI_APPLICATION = "setup.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.mysql",
-        "NAME": "Shah123$default",
-        "USER": "Shah123",
-        "PASSWORD": "Admin(123)",
-        "HOST": "Shah123.mysql.pythonanywhere-services.com",
-        "PORT": "3306",
-    }
-}
-
 # DATABASES = {
 #     "default": {
-#         "ENGINE": "django.db.backends.sqlite3",
-#         "NAME": BASE_DIR / "db.sqlite3",
+#         "ENGINE": "django.db.backends.mysql",
+#         "NAME": "Shah123$default",
+#         "USER": "Shah123",
+#         "PASSWORD": "Admin(123)",
+#         "HOST": "Shah123.mysql.pythonanywhere-services.com",
+#         "PORT": "3306",
 #     }
 # }
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
+}
 
 
 # Password validation
