8cc031926eefaa1c0ac2eda36e4d4592de518e98
Umair Ashraf <umairashraf5252@gmail.com>
2025-05-04 21:39:04 +0500

    fixed

--- setup/settings.py
+++ setup/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "0z^!14-*)rxw#b#c7^#axlgw#0n!k^fxxasj_3u%$b$-bx(8u1"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
